a93fa0b82ae15431103a033d26ad528da5e8c122
Duncan Kioi <duncan@griffinkenya.com>
2023-01-26 14:01:07 +0300

    login page done. Internet permission added and retrofit dependency added

--- app/src/main/java/com/kenyans/apiproject/MainActivity.java
+++ app/src/main/java/com/kenyans/apiproject/MainActivity.java
@@ -2,13 +2,33 @@ package com.kenyans.apiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout textInputLayout;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInputLayout = findViewById(R.id.textInput);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,User_Activity.class);
+                i.putExtra("String_that_I_NEED", textInputLayout.getEditText().getText().toString());
+                startActivity(i);
+            }
+        });
     }
 }
